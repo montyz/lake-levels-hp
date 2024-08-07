ab6e7360c06aac1f34521fa0db38fe0bf10e5064
Monty Zukowski <monty@washdish.com>
2024-08-07 10:16:30 -0700

    lake level test

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
-@st.cache_data
+@st.cache_data(ttl='8h')
 def get_gdp_data():
     """Grab GDP data from a CSV file.
 
@@ -24,35 +24,6 @@ def get_gdp_data():
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
     raw_gdp_df = pd.read_csv('https://www.usbr.gov/pn-bin/daily.pl?station=hpd&format=csv&year=2024&month=6&day=30&year=2024&month=8&day=31&pcode=fb&pcode=qj')
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    # gdp_df = raw_gdp_df.melt(
-    #     ['Country Code'],
-    #     [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-    #     'Year',
-    #     'GDP',
-    # )
-
-    # # Convert years from string to integers
-    # gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
     return raw_gdp_df
 
 gdp_df = get_gdp_data()
@@ -71,80 +42,6 @@ by Monty Zukowski
 ''
 ''
 
-# min_value = gdp_df['Year'].min()
-# max_value = gdp_df['Year'].max()
-
-# from_year, to_year = st.slider(
-#     'Which years are you interested in?',
-#     min_value=min_value,
-#     max_value=max_value,
-#     value=[min_value, max_value])
-
-# countries = gdp_df['Country Code'].unique()
-
-# if not len(countries):
-#     st.warning("Select at least one country")
-
-# selected_countries = st.multiselect(
-#     'Which countries would you like to view?',
-#     countries,
-#     ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-# ''
-# ''
-# ''
-
-# # Filter the data
-# filtered_gdp_df = gdp_df[
-#     (gdp_df['Country Code'].isin(selected_countries))
-#     & (gdp_df['Year'] <= to_year)
-#     & (from_year <= gdp_df['Year'])
-# ]
-
-# st.header('GDP over time', divider='gray')
-
-# ''
-
-# st.line_chart(
-#     filtered_gdp_df,
-#     x='Year',
-#     y='GDP',
-#     color='Country Code',
-# )
-
-# ''
-# ''
-
-
-# first_year = gdp_df[gdp_df['Year'] == from_year]
-# last_year = gdp_df[gdp_df['Year'] == to_year]
-
-# st.header(f'GDP in {to_year}', divider='gray')
-
-# ''
-
-# cols = st.columns(4)
-
-# for i, country in enumerate(selected_countries):
-#     col = cols[i % len(cols)]
-
-#     with col:
-#         first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-#         last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-#         if math.isnan(first_gdp):
-#             growth = 'n/a'
-#             delta_color = 'off'
-#         else:
-#             growth = f'{last_gdp / first_gdp:,.2f}x'
-#             delta_color = 'normal'
-
-#         st.metric(
-#             label=f'{country} GDP',
-#             value=f'{last_gdp:,.0f}B',
-#             delta=growth,
-#             delta_color=delta_color
-#         )
 ramp = st.number_input('ramp elevation', value = 4501.0)
 gdp_df['hpd_fb'] = gdp_df['hpd_fb'] - ramp
 
